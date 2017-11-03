0ba1f3286755dc46edd9aa484bc6a04d2ddd6da8
Jin Liu <pennsylvaniajin@gmail.com>
2017-11-03 15:55:07 -0400

    Create GlyphLypht Subsystem class and constant
    Well, the constant's almost done, I'm not sure what to do here lel

--- Team9889/src/main/java/com/team9889/Constants.java
+++ Team9889/src/main/java/com/team9889/Constants.java
@@ -9,6 +9,12 @@ public class Constants {
     //VuMark Licence Key
     public final static String kVuforiaLicenceKey = "AUEjQhP/////AAAAGV8qq1GGcE03gQHKWYPRZBNIZYZq8DoUn9lOpeWPY8PZKG1B5UtrsqkqPqh8Vcuv+HuUuobUNZE35fhYNPqxRIWqtoKbUmkZZCspmu0Aw685D5dxk87dT38/oYxzdKFs3EZaD8hvprmWj2Oww4+GtxS+fiImc23ZlkU20esE1MhwvX0xJ8tjoPS9pdTVSt1QtoYp3WxSxtZlKd0B0UMCxzj0KxN4JZRlTmF2W3dLU2G9SJ3hQO8jcC+Nuvbfk809C6LSWiijZ9L7IZNcQQiFKDa5yJP+ayX+Y1cpwcV19yqlPQTH7CQqcvnZDfwQZXmCpDhcQpW9h+bCXaerRH/uWNZMskyO0AXeFa1oCgB3EGPB";
 
+    /*------------------------
+     |                       |
+     |    DRIVETRAIN!!11!!1  |
+     |                       |
+     -------------------------
+     */
     //Settings for MRDrive class
     public final static String kLeftDriveMasterId = "LDrive1";
     public final static String kLeftDriveSlaveId = "LDrive2";
@@ -33,6 +39,13 @@ public class Constants {
     //Distance between wheels
     public final static double TrackWidth = 16;
 
+    /*---------------------------
+    |                           |
+    |   Glyph Lypht!!!11!11!    |
+    |                           |
+    ---------------------------*/
+
+    public final static String GlyphLypht = "GlyphLyft";
     public static double ticksToInches(int ticks){
         return ticks/Constants.CountsPerInch;
     }
---
+++ Team9889/src/main/java/com/team9889/subsystems/GlyphLypht.java
@@ -0,0 +1,8 @@
+package com.team9889.subsystems;
+
+/**
+ * Created by Jin on 10/27/2017.
+ */
+
+public class GlyphLypht {
+}
