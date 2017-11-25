5be6baffc76ef7ac1db1882a7d49468037b0cbb7
joshua9889 <heinrich.joshua@student.ccm.edu>
2017-11-25 18:20:14 -0500

    Added "Fail Proof" Autons

---
+++ Team9889/src/main/java/com/team9889/auto/modes/Blue.java
@@ -0,0 +1,22 @@
+package com.team9889.auto.modes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.team9889.auto.AutoModeBase;
+import com.team9889.auto.actions.JewelHitColor;
+
+/**
+ * Created by joshua9889 on 11/25/2017.
+ */
+
+@Autonomous(name = "RED FAIL PROOF")
+public class Red extends AutoModeBase{
+    @Override
+    public void runOpMode(){
+        waitForTeamStart(this, true);
+        runAction(new JewelHitColor(JewelColor.Red));
+        sleep(1000);
+        Robot.getJewel().stop();
+        sleep(1000);
+    }
+}
---
+++ Team9889/src/main/java/com/team9889/auto/modes/Red.java
@@ -0,0 +1,8 @@
+package com.team9889.auto.modes;
+
+/**
+ * Created by joshua9889 on 11/25/2017.
+ */
+
+public class Red {
+}
