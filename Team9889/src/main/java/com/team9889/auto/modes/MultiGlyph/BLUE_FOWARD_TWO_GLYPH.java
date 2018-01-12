9e8e6870746cf31e74eb1b3def0a864f14605ca5
joshua9889 <heinrich.joshua@student.ccm.edu>
2018-01-12 14:44:28 -0500

    Update Blue Foward 2 Glyph

--- Team9889/src/main/java/com/team9889/auto/modes/MultiGlyph/BLUE_FOWARD_TWO_GLYPH.java
+++ Team9889/src/main/java/com/team9889/auto/modes/MultiGlyph/BLUE_FOWARD_TWO_GLYPH.java
@@ -4,8 +4,10 @@ import com.team9889.auto.AutoModeBase;
 import com.team9889.auto.actions.DriveTimeAction;
 import com.team9889.auto.actions.DriveToDistance;
 import com.team9889.auto.actions.GlyphDeployToFirstLevel;
+import com.team9889.auto.actions.GlyphDeployToFirstLevelTwoGlyph;
 import com.team9889.auto.actions.GlyphRelease;
 import com.team9889.auto.actions.GlyphRetractArm;
+import com.team9889.auto.actions.GlyphStorePreload;
 import com.team9889.auto.actions.IntakeDeployAndCollect;
 import com.team9889.auto.actions.IntakePull;
 import com.team9889.auto.actions.IntakeSwivel;
@@ -20,75 +22,29 @@ import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 public class BLUE_FOWARD_TWO_GLYPH {
     public BLUE_FOWARD_TWO_GLYPH(AutoModeBase M, RelicRecoveryVuMark column){
         // Drive off balance stone
-        M.runAction(new DriveToDistance(21, 0));
+        M.runAction(new DriveToDistance(18, 0));
 
-        // Turn to the cryptobox
-        M.runAction(new TurnToAngle(90));
+        M.runAction(new GlyphStorePreload());
 
-        // Deploy Lift
-        M.ThreadAction(new GlyphDeployToFirstLevel());
-
-        // Drive to cryptobox
-        M.runAction(new DriveToDistance(30, 91));
-
-        // Release Glyph
-        M.runAction(new GlyphRelease());
-
-        // Back away from cryptobox
-        M.runAction(new DriveToDistance(-10, 90));
-
-        // Retract Lift and Deploy Intake
-        M.ThreadAction(new GlyphRetractArm());
-        M.ThreadAction(new IntakeDeployAndCollect());
-
-        // Turn to Glyph Pit
-        M.runAction(new TurnToAngle(-60));
-
-        // Drive into Glyph Pit
-        M.runAction(new DriveToDistance(28, -60));
-
-        // Wait a little
-        M.sleep(400);
-
-        // Pull any glyphs in
-        M.runAction(new IntakePull());
-        M.sleep(250);
-
-        // Straighten Glyph out
-        M.runAction(new IntakeSwivel());
-
-        // Deploy Intake
         M.runAction(new IntakeDeployAndCollect());
 
-        // Drive a little farther forward
-        M.runAction(new DriveToDistance(4, -60));
+        M.runAction(new TurnToAngle(-45));
 
-        // Pull any glyphs in
-        M.runAction(new IntakePull());
-        M.sleep(100);
+        M.runAction(new DriveToDistance(12, -45, 5*Math.PI));
+        M.sleep(1000);
 
-        // Back away from Glyph Pit
-        M.runAction(new DriveToDistance(-26, -60));
+        M.Robot.getLift().setServoPosition(0.4);
+        M.Robot.getLift().clamp();
+        M.Robot.getIntake().stopIntake();
+        M.sleep(400);
 
-        // Straighten Glyphs out
-        M.ThreadAction(new IntakeSwivel());
+        M.runAction(new DriveToDistance(-12, -45));
 
-        // Turn to Cryptobox
-        M.runAction(new TurnToAngle(115));
+        M.runAction(new TurnToAngle(93));
+        M.ThreadAction(new GlyphDeployToFirstLevelTwoGlyph());
+        M.runAction(new DriveTimeAction(2000, 2*Math.PI, 93));
 
-        // Deploy Lift
-        M.ThreadAction(new GlyphDeployToFirstLevel());
-        M.sleep(100);
-
-        // Drive to Crytobox
-        M.runAction(new DriveTimeAction(900, 5*Math.PI, 115));
-
-        // Release Any glyphs into Cryptobox
         M.runAction(new GlyphRelease());
-
-        // Back up, then ram, and then back up.
-        M.runAction(new DriveToDistance(-2, 115));
-        M.runAction(new DriveTimeAction(300, 4*Math.PI, 115));
-        M.runAction(new DriveToDistance(-3, 115));
+        M.runAction(new DriveToDistance(-4, 93, Math.PI));
     }
 }
--- Team9889/src/main/java/com/team9889/test/Autonomus/FixBrokenThings.java
+++ Team9889/src/main/java/com/team9889/test/Autonomus/FixBrokenThings.java
@@ -24,23 +24,7 @@ public class FixBrokenThings extends AutoModeBase {
         jewel_Color = JewelColor.Blue;
         runAction(new JewelHitColor(JewelColor.Blue));
 
-        runAction(new DriveToDistance(18, 0));
-        runAction(new GlyphStorePreload());
-        runAction(new IntakeDeployAndCollect());
-        runAction(new TurnToAngle(-45));
-        runAction(new DriveToDistance(12, -45, 5*Math.PI));
-        sleep(1000);
-        Robot.getLift().setServoPosition(0.4);
-        Robot.getLift().clamp();
-        Robot.getIntake().stopIntake();
-        sleep(400);
-        runAction(new DriveToDistance(-12, -45));
-        runAction(new TurnToAngle(93));
 
-        ThreadAction(new GlyphDeployToFirstLevelTwoGlyph());
-        runAction(new DriveTimeAction(2000, 2*Math.PI, 93));
-        runAction(new GlyphRelease());
-        runAction(new DriveToDistance(-4, 93, Math.PI));
 
 
     }
